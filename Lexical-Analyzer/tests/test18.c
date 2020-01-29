afa7255646c685c1ba4c89ca5efa154d0b31beba
njnisarg <NJnisarg@gmail.com>
2020-01-30 00:22:53 +0530

    feat: lexical analyzer done

---
+++ Lexical-Analyzer/tests/test18.c
@@ -0,0 +1,12 @@
+#define VAL 5
+
+int main()
+{
+    int i=0;
+    for(i=0;i<VAL;i++)
+    {
+        printf("Hello \n");
+    }
+}
+
+// Testcase for Macro and loop
---
+++ Lexical-Analyzer/tests/test21.c
@@ -0,0 +1,8 @@
+/* C program for Merge Sort */
+#include<stdlib.h
+#include<stdio.h>
+  
+int main()
+{
+    printf("\n Hello World \n");
+}
